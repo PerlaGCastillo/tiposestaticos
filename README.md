753e5d334694c640483e94921825ceaaf03ef35d
PerlaGodinez <perla.godinez@stps.gob.mx>
2024-03-12 13:19:47 -0600

    project structure config

--- README.md
+++ README.md
@@ -3,11 +3,26 @@ api rest con tipos estáticos, Typescript, nodeJs, Express
 
 
 
+#run
+```
+git clone -
+install node
+npm init -y
 
 ```
+
+
+#notas durante el desarrollo
+```
+install node
+
 npm init -y
 npm i typescript -D
+npm run tsc -- --init
+npm install express -E
 
+npm i @types/express -D
 
-
+levantar y compilar, ejecutar cambios 
+$ npm i ts-node-dev -D
 ```
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'// ESModules
+import diaryRouter from './routes/diaries'
+
+const app = express()
+app.use(express.json())// middleware que transforma la req.body a un json
+
+const PORT = 3000
+
+app.get('/ping', (_, res) => {
+    console.log('someone pinged here! ' + new Date().toLocaleDateString())
+    res.send('pong')
+})
+
+app.use('/api/diaries', diaryRouter)
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
